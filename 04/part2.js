dd60152e61f710c0246bce996a6aebe609676a4a
PiMastah <pimastah@gmail.com>
2022-12-04 06:30:12 +0100

    [FEAT] Day 04

---
+++ 04/part2.js
@@ -0,0 +1,16 @@
+import { EOL } from 'os'
+import { readFileSync } from 'fs'
+
+const input = 
+  readFileSync('input.txt', 'utf8')
+  .split(EOL)
+  .map(x => x.split(',').map(y => y.split('-').map(i => +i)))
+
+console.log(
+  input.filter(e =>
+    e[0][0] >= e[1][0] && e[0][0] <= e[1][1]
+    || e[0][1] >= e[1][0] && e[0][1] <= e[1][1]
+    || e[1][0] >= e[0][0] && e[1][0] <= e[0][1]
+    || e[1][1] >= e[0][0] && e[1][1] <= e[0][1]
+  ).length
+)
