db859d58aaffc2b8a5ba045e27937ff5c9904f58
PiMastah <pimastah@gmail.com>
2022-12-11 06:24:55 +0100

    [FEAT] Day 11

---
+++ 11/part1.js
@@ -0,0 +1,40 @@
+import { EOL } from 'os'
+import { readFileSync } from 'fs'
+
+const input =
+  readFileSync('input.txt', 'utf8')
+  .split(EOL+EOL).map(m => m.split(EOL))
+
+const monkeys = input.map(parts => {
+  const op = old => {
+    return eval(parts[2].split('=')[1])
+  }
+  return {
+    'id': +parts[0].split(' ')[1].slice(0,-1),
+    'items': parts[1].split(':')[1].trim().split(', ').map(x => +x),
+    op,
+    'c': +parts[3].split(' ').at(-1),
+    't': +parts[4].split(' ').at(-1),
+    'f': +parts[5].split(' ').at(-1),
+    'thrown': 0
+  }
+})
+
+let rounds = 20
+
+while (rounds-- > 0) {
+  monkeys.forEach(m => {
+    m.items.slice(0).forEach(i => {
+      const wl = Math.floor(m.op(i) / 3)
+      m.items.shift()
+      if (wl % m.c == 0) {
+        monkeys[m.t].items.push(wl)
+      } else {
+        monkeys[m.f].items.push(wl)
+      }
+      m.thrown += 1
+    })
+  })
+}
+
+console.log(monkeys.sort((a,b) => b.thrown - a.thrown).slice(0, 2).reduce((p,c) => p*c.thrown, 1))
